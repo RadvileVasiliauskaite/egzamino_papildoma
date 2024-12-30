491a438a09a430aa411d23e31631deaa850a9a47
RadvileVasiliauskaite <radvilevas@gmail.com>
2024-12-30 15:12:54 +0200

    Papildytas input.txt

--- funkcijos.h
+++ funkcijos.h
@@ -6,17 +6,9 @@
 #include <map>
 #include <set>
 
-void countWordsAndLines(const std::string& filename,
-    std::map<std::string, int>& wordCounts,
-    std::map<std::string, std::set<int>>& wordLines,
-    std::map<std::string, std::string>& originalWords);
-void writeWordCounts(const std::string& outputFilename,
-    const std::map<std::string, int>& wordCounts,
-    const std::map<std::string, std::string>& originalWords);
-void writeCrossReference(const std::string& outputFilename,
-    const std::map<std::string, std::set<int>>& wordLines,
-    const std::map<std::string, int>& wordCounts,
-    const std::map<std::string, std::string>& originalWords);
+void countWordsAndLines(const std::string& filename, std::map<std::string, int>& wordCounts, std::map<std::string, std::set<int>>& wordLines, std::map<std::string, std::string>& originalWords);
+void writeWordCounts(const std::string& outputFilename, const std::map<std::string, int>& wordCounts, const std::map<std::string, std::string>& originalWords);
+void writeCrossReference(const std::string& outputFilename, const std::map<std::string, std::set<int>>& wordLines, const std::map<std::string, int>& wordCounts, const std::map<std::string, std::string>& originalWords);
 void findURLs(const std::string& filename, std::set<std::string>& urls);
 void showMenu();
 
